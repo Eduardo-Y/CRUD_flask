7bd6179c515bca4aa195b50394e6e630bb9b7512
Yoda <yoda@Yoda.fiber10telecom.com.br>
2025-05-02 15:46:21 -0300

    Implementando a opção de editar dados de usúarios

--- routes/users.py
+++ routes/users.py
@@ -8,19 +8,33 @@ def list_users():
     return render_template('users.html', users=USERS)
 
 @user_route.route('/create-user', methods=['POST'])
-def new_user():
+def form_insert_user():
     name = request.form['name']
     email = request.form['email']
     id = len(USERS) + 1
     USERS.append({'id':id, 'name':name, 'email':email})
-    return render_template('form_new_user.html')
+    return render_template('form_insert_user.html')
 
 @user_route.route('/create-user')
 def insert_user():
-    return render_template('form_new_user.html')
+    return render_template('form_insert_user.html')
 
 @user_route.route('/<int:id>/delete', methods=['POST'])
 def delete_user(id):
     global USERS
     USERS = [u for u in USERS if u['id'] != id]
-    return render_template('users.html', users=USERS)
+    return list_users()
+
+@user_route.route('/<int:id>/update')
+def form_update_user(id):
+    global USERS
+    old_name = USERS[id]['name']
+    old_email = USERS[id]['email']
+    return render_template('form_update_user.html', users=USERS, id=id, old_name=old_name, old_email=old_email)
+
+@user_route.route('/<int:id>/update', methods=['POST'])
+def update_user(id):
+    global USERS
+    USERS[id]['name'] = request.form['name']
+    USERS[id]['email'] = request.form['email']
+    return list_users()
