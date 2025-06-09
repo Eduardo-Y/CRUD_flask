840dd36e3e1d3b8692696bf680e4c058566874f4
Yoda <yoda@Yoda.fiber10telecom.com.br>
2025-06-09 13:09:06 -0300

    Corrigindo Erro no Upadate de users

--- routes/users.py
+++ routes/users.py
@@ -28,13 +28,18 @@ def delete_user(id):
 @user_route.route('/<int:id>/update')
 def form_update_user(id):
     global USERS
-    old_name = USERS[id]['name']
-    old_email = USERS[id]['email']
+    for u in USERS:
+        if u['id'] == id:
+            old_name = u['name']
+            old_email = u['email']
+
     return render_template('form_update_user.html', users=USERS, id=id, old_name=old_name, old_email=old_email)
 
 @user_route.route('/<int:id>/update', methods=['POST'])
 def update_user(id):
     global USERS
-    USERS[id]['name'] = request.form['name']
-    USERS[id]['email'] = request.form['email']
+    for u in USERS:
+        if u['id'] == id:
+            u['name'] = request.form['name']
+            u['email'] = request.form['email']
     return list_users()
